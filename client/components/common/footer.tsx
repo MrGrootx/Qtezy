48056bc32be702b8f8ebb7e7496e3d5b5122675f
justgroot <mrgrootx8@gmail.com>
2025-07-09 15:37:00 +0530

    feat: footer

--- client/components/common/footer.tsx
+++ client/components/common/footer.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
-
+import React from "react";
+import { Separator } from "@/components/ui/separator";
+import { Button } from "../ui/button";
+import { Github, Instagram } from "lucide-react";
+import Link from "next/link";
+import Image from "next/image";
 const footer = () => {
   return (
-    <div>footer</div>
-  )
-}
+    <div className="container mx-auto px-4 w-full">
+      <Separator />
+      <div className="mt-8 mb-8 flex items-center justify-between space-y-2 w-full">
+        <div>
+          <Link href="/" className="flex items-center gap-1 mb-1">
+            <Image src={"/logo.png"} alt="Qtezy Logo" width={20} height={20} />
+            <small className="font-semibold">tezy</small>
+          </Link>
+          <p className="text-sm text-gray-500 dark:text-gray-400">
+            Weekly wisdom. Pure inspiration, no spam.
+          </p>
+        </div>
+        <div className="space-x-2">
+          <Link href={"https://github.com/MrGrootx"} target="_blank">
+            <Button variant="outline" size={"icon"} className="cursor-pointer">
+              <Instagram />
+            </Button>
+          </Link>
+          <Link href={"https://github.com/MrGrootx"} target="_blank">
+            <Button variant="outline" size={"icon"} className="cursor-pointer">
+              <Github />
+            </Button>
+          </Link>
+        </div>
+      </div>
+      <Separator />
+      <div className="mt-8 mb-8 flex items-center justify-between space-y-2 w-full">
+        <small className="text-sm text-gray-500 dark:text-gray-400 ">
+          © {new Date().getFullYear()} Qtezy. All rights reserved.
+        </small>
+        <small className="text-sm text-gray-500 dark:text-gray-400 ">
+          Made with ❤️ for quote lovers everywhere
+        </small>
+      </div>
+    </div>
+  );
+};
 
-export default footer
+export default footer;
